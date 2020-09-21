5f64c4194fc8489fdcd807a25baca3cc323fb594
Hanwe Lee <mobiledev@kakao.com>
2020-09-21 21:43:22 +0900

    alamofire wrapping class 수정

--- Manager/DataApiManager.swift
+++ Manager/DataApiManager.swift
@@ -11,54 +11,8 @@ import SwiftyJSON
 import Alamofire
 
 class DataApiManager: NSObject {
-    
-    class func requestGETURL(_ strURL: String, headers : [String : String]?, success:@escaping(JSON)-> Void, failure:@escaping (Error) -> Void) {
-        var httpHeaders:HTTPHeaders? = nil
-        if headers != nil {
-            httpHeaders = HTTPHeaders.init(headers!)
-        }
-        AF.request(strURL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: httpHeaders).validate().responseJSON { (responseObject) -> Void in
-//            print(responseObject)
-            switch responseObject.result {
-            case .success(let value):
-//                print("value :\(value)")
-                let resJson = JSON(value)
-                success(resJson)
-                break
-            case .failure(let error):
-//                print("error :\(error)")
-                let errorValue : Error = error
-                failure(errorValue)
-                break
-            }
-        }
-    }
-    
-    class func requestPOSTURL(_ strURL : String, params : Parameters = [:], headers : [String : String]?, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void){
-        var httpHeaders:HTTPHeaders? = nil
-        if headers != nil {
-            httpHeaders = HTTPHeaders.init(headers!)
-        }
-        AF.request(strURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: httpHeaders).validate().responseJSON { (responseObject) -> Void in
 
-            //print(strURL)
-
-            switch responseObject.result {
-            case .success(let value):
-                //                print("value :\(value)")
-                let resJson = JSON(value)
-                success(resJson)
-                break
-            case .failure(let error):
-                //                print("error :\(error)")
-                let errorValue : Error = error
-                failure(errorValue)
-                break
-            }
-        }
-    }
-
-    class func requestPOSTURLTypeReturn(_ strURL : String, params : Parameters = [:], headers : [String : String]?, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) -> DataRequest{
+    @discardableResult class func requestPOSTURL(_ strURL : String, params : Parameters = [:], headers : [String : String]?, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) -> DataRequest{
         var httpHeaders:HTTPHeaders? = nil
         if headers != nil {
             httpHeaders = HTTPHeaders.init(headers!)
@@ -83,7 +37,7 @@ class DataApiManager: NSObject {
     }
 
     
-    class func requestGETURLTypeReturn(_ strURL: String, headers : [String : String]?, success:@escaping(JSON)-> Void, failure:@escaping (Error) -> Void) -> DataRequest {
+    @discardableResult class func requestGETURL(_ strURL: String, headers : [String : String]?, success:@escaping(JSON)-> Void, failure:@escaping (Error) -> Void) -> DataRequest {
         var httpHeaders:HTTPHeaders? = nil
         if headers != nil {
             httpHeaders = HTTPHeaders.init(headers!)
