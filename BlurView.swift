2497074012fb20c149ba8ef55986785cc6ac13a7
LEE HANWE <mobiledev@kakao.com>
2020-12-30 09:02:15 +0900

    Update BlurView.swift
    blur view 수정

--- BlurView.swift
+++ BlurView.swift
@@ -1,61 +1,49 @@
 //
 //  BlurView.swift
-//  hanwe
+//  BlurViewTest
 //
-//  Created by hanwe on 2020. 12. 15..
-//  Copyright © 2020년 hanwe. All rights reserved.
+//  Created by hanwe lee on 2020/12/30.
+//  Copyright © 2020 hanwe. All rights reserved.
 //
 
 import UIKit
-class BlurView: UIView {
 
-    struct ScreenSize  {
-        static let Width         = UIScreen.main.bounds.size.width
-        static let Height        = UIScreen.main.bounds.size.height
-        static let Max_Length    = max(ScreenSize.Width, ScreenSize.Height)
-        static let Min_Length    = min(ScreenSize.Width, ScreenSize.Height)
+class BackgroundModeBlurView: UIView {
+    
+    //MARK: IBOutlet
+    
+    //MARK: life cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initView()
+    }
+    //MARk: internal function
+    
+    internal func initView() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.willResignActiveNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.didBecomeActiveNotification, object: nil)
+        makeBlurView()
+        self.layer.masksToBounds = true
+        self.isHidden = true
     }
     
-    static let shared: BlurView = {
-        
-        let instance = BlurView()
-        instance.frame = UIScreen.main.bounds
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        instance.isHidden = true
-        appDelegate.window?.addSubview(instance)
-        instance.makeBlurView()
-        
-        return instance
-        
-    }()
+    //MARk: private function
     
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        self.backgroundColor = UIColor(red:0, green:0, blue:0, alpha:1).withAlphaComponent(0.2)
-
+    @objc private func appMovedToBackground() {
+        self.isHidden = false
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    @objc private func appMovedToForeground() {
+        self.isHidden = true
     }
     
-    func makeBlurView() {
-        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+    private func makeBlurView() {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = self.bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(blurEffectView)
     }
-    
-    func showBlurView() {
-        self.isHidden = false
-    }
-    
-    func hide() {
-        self.isHidden = true
-        
-    }
-    
+
 }
