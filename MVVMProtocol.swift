d7b01a2165f70b93fc1ae32860d0bfa270adf4c0
LEE HANWE <mobiledev@kakao.com>
2021-02-23 13:53:00 +0900

    Update MVVMProtocol.swift

--- MVVMProtocol.swift
+++ MVVMProtocol.swift
@@ -10,6 +10,7 @@ import RxSwift
 
 protocol MVVMViewController: class {
     associatedtype MVVMViewModelClassType: MVVMViewModel
+    associatedtype SelfType: MVVMViewController
     
     var disposeBag: DisposeBag { get set }
     var isViewModelBinded: Bool { get set }
@@ -17,10 +18,34 @@ protocol MVVMViewController: class {
     
     func bind(viewModel: MVVMViewModel)
     func bindingViewModel<T: MVVMViewModel>(viewModel: T) -> T
+    
+    static func makeViewController(viewModel: MVVMViewModelClassType) -> SelfType?
 }
 
 extension MVVMViewController {
+    @discardableResult func bindingViewModel<T: MVVMViewModel>(viewModel: T) -> T {
+        bind(viewModel: viewModel)
+        viewModel.subscribeInputs()
+        self.isViewModelBinded = true
+        return viewModel
+    }
+}
+
+protocol MVVMView: class {
+    associatedtype MVVMViewModelClassType: MVVMViewModel
+    associatedtype SelfType: MVVMView
     
+    var disposeBag: DisposeBag { get set }
+    var isViewModelBinded: Bool { get set }
+    var viewModel: MVVMViewModelClassType! { get set }
+    
+    func bind(viewModel: MVVMViewModel)
+    func bindingViewModel<T: MVVMViewModel>(viewModel: T) -> T
+    
+    static func makeView(viewModel: MVVMViewModelClassType) -> SelfType?
+}
+
+extension MVVMView {
     @discardableResult func bindingViewModel<T: MVVMViewModel>(viewModel: T) -> T {
         bind(viewModel: viewModel)
         viewModel.subscribeInputs()
@@ -34,3 +59,4 @@ protocol MVVMViewModel: class {
     
     func subscribeInputs()
 }
+
