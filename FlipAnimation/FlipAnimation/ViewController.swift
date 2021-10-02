7ac86963b4d4120a8cb55f71d185ed8be7a71441
LEE HANWE <mobiledev@kakao.com>
2021-10-02 22:01:52 +0900

    Flip
    Flip

---
+++ FlipAnimation/FlipAnimation/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  FlipAnimation
+//
+//  Created by hanwe on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController, HWFlipViewDelegate {
+    func flipViewWillFliped(flipView: HWFlipView, foregroundView: UIView, behindeView: UIView, willShow: HWFlipView.FlipType) {
+//        print("flipView: \(flipView), foregroundView: \(foregroundView), behindeView: \(behindeView), willShow: \(willShow)")
+    }
+    
+    func flipViewDidFliped(flipView: HWFlipView, foregroundView: UIView, behindeView: UIView, didShow: HWFlipView.FlipType) {
+//        print("flipView: \(flipView), foregroundView: \(foregroundView), behindeView: \(behindeView), willShow: \(didShow)")
+    }
+    
+
+    @IBOutlet weak var flipView: HWFlipView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let front: UIView = UIView()
+//        front.backgroundColor = .yellow
+//        let back: UIView = UIView()
+//        back.backgroundColor = .darkGray
+//        self.flipView.setForegroundView(front)
+//        self.flipView.setBehindViewView(back)
+        
+        let front: UIImageView = UIImageView()
+        front.image = UIImage(named: "header")
+        let back: UIImageView = UIImageView()
+        back.image = UIImage(named: "tomato")
+        self.flipView.setForegroundView(front)
+        self.flipView.setBehindViewView(back)
+        
+        flipView.delegate = self
+    }
+    
+    @IBAction func flipAction(_ sender: Any) {
+        self.flipView.flip(complition: {
+            print("애니메이션 끝")
+        })
+    }
+    
+
+}
+
