a6f43306782550e33133d2a58f72f489a2e94b91
LEE HANWE <mobiledev@kakao.com>
2022-06-30 17:44:00 +0900

    Update BlurView.swift

--- BlurView.swift
+++ BlurView.swift
@@ -6,44 +6,84 @@
 //  Copyright © 2020 hanwe. All rights reserved.
 //
 
-import UIKit
+import SnapKit
+import Then
 
-class BackgroundModeBlurView: UIView {
-    
-    //MARK: IBOutlet
-    
-    //MARK: life cycle
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        initView()
+protocol SecureInactiveView where Self: UIViewController {
+    func registSecureCoverView()
+}
+
+private var secureInactiveViewAssociatedObjectKey: Void?
+
+extension SecureInactiveView {
+    private var secureCoverView: SecureInactiveCoverView {
+        get {
+            var coverView = objc_getAssociatedObject(self, &secureInactiveViewAssociatedObjectKey) as? SecureInactiveCoverView
+            if coverView == nil {
+                coverView = SecureInactiveCoverView()
+                objc_setAssociatedObject(self, &secureInactiveViewAssociatedObjectKey, coverView, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+            return coverView ?? SecureInactiveCoverView()
+        }
+        set {
+            objc_setAssociatedObject(self, &secureInactiveViewAssociatedObjectKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
     }
-    //MARk: internal function
     
-    internal func initView() {
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.willResignActiveNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(appMovedToForeground), name: UIApplication.didBecomeActiveNotification, object: nil)
+    func registSecureCoverView() {
+        NotificationCenter.default.addObserver(forName: Notification.Name(NotificationDefine.APP_DID_BECOME_ACTIVE_NOTIFICATION_DEFINE), object: nil, queue: nil) { [unowned self] notification in
+            becomeActive(notification: notification)
+        }
+        NotificationCenter.default.addObserver(forName: Notification.Name(NotificationDefine.APP_WILL_RESIGN_ACTIVE_NOTIFICATION_DEFINE), object: nil, queue: nil) { [unowned self] notification in
+            resignActive(notification: notification)
+        }
+        
+        self.view.addSubview(secureCoverView)
+        self.secureCoverView.snp.makeConstraints {
+            $0.edges.equalTo(self.view)
+        }
+        self.secureCoverView.isHidden = true
+    }
+    
+    private func becomeActive(notification: Notification) {
+        self.secureCoverView.isHidden = true
+    }
+    
+    private func resignActive(notification: Notification) {
+        self.secureCoverView.isHidden = false
+    }
+}
+
+class SecureInactiveCoverView: UIView {
+    
+    // MARK: private UI property
+    
+    // MARK: internal UI property
+    
+    // MARK: private property
+    
+    // MARK: property
+    
+    // MARK: lifeCycle
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         makeBlurView()
-        self.layer.masksToBounds = true
-        self.isHidden = true
     }
     
-    //MARk: private function
-    
-    @objc private func appMovedToBackground() {
-        self.isHidden = false
-    }
-    
-    @objc private func appMovedToForeground() {
-        self.isHidden = true
-    }
+    // MARK: private func
     
     private func makeBlurView() {
-        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+        let blurEffect = UIBlurEffect(style: .regular)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = self.bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(blurEffectView)
     }
-
+    
+    // MARK: func
 }
